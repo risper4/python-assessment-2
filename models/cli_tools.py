d1cbab8b022ef19c9b1c9b00498b8449a976aa4e
Risper <risperwanjikugichia@gmail.com>
2026-06-12 12:49:42 +0300

    Project class model

--- models/cli_tools.py
+++ models/cli_tools.py
@@ -13,9 +13,27 @@ class User :
     
     def add_project(self, project) :
         self.projects.append(project)
-        print(f"📌 Task '{project.title}' has been added to {self.name}'s project list")
+        print(f"📌 Project '{project.title}' has been added to {self.name}'s project list")
+
+
+class Project :
+    project_list = []
+    def __init__(self, title, due_date):
+        self.title = title
+        self.due_date = due_date
+        self.tasks = []
+        Project.project_list.append(self)
+
+    def add_task(self, task) :
+        self.tasks.append(task)
+        print(f" 📌 Task '{task.title}' is added to {self.title}'s to-do list")
+
+
+
 
 
 # user1 = User('Maya', 'maya@gmail.com')
 # user2 = User('James', 'james@gmail.com')
+# project1 = Project('Research', '23/3/2026')
 # user1.list_users()
+# user1.add_project(project1)
